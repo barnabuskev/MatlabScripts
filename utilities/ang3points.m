4f2621b0c1fedb46cebdc45e72411fec98c39fdd
Kevin Brownhill <kevin.brownhill@gmail.com>
2017-01-13 15:58:58 +0000

    refactoring

--- utilities/ang3points.m
+++ utilities/ang3points.m
@@ -1,26 +1,23 @@
-function theta = ang3points(p3)
-%function that given a 2X3 or 3X2 matrix returns the angle in degrees subtended
-%by clockwise rotation between the 1st straight-line segment & the second
-sz = size(p3);
+function theta = ang3points(pts3)
+% function that given a 2X3 or 3X2 matrix of x, y coordinates returns the
+% angle in degrees between the vector made up of the first two coordinates
+% and that made up of the last two coords (rotate first towards second clockwise)
+% Assumes Cartesian coords
+
+% CHECK INPUT
+sz = size(pts3);
 if ~(min(sz) == 2 && max(sz) == 3)
     error('size of input must be 2X3 or 3X2')
 end
 if sz(1) == 2
-    p3 = p3';
+    pts3 = pts3';
 end
-v1 = p3(1,:) - p3(2,:);
-v2 = p3(3,:) - p3(2,:);
-%get angle of v1 from (1,0)
-v1a = atan2(v1(2),v1(1));
-%rotate v2 back by this amount (i.e. clockwise)
-rot = [cos(-v1a),-sin(-v1a);sin(-v1a),cos(-v1a)];
-v2 = (rot * v2')';
-%get angle of transformed v2 from (1,0)
-v2a = atan2(v2(2),v2(1));
-%convert to clockwise rotation angle
-if v2a > 0
-    v2a = 2 * pi - v2a;
-else
-    v2a = -v2a;
-end
-theta = v2a * 180 / pi;
+
+% GET ANGLE
+% translate points so that middle point is at the origin
+pts3 = bsxfun(@minus,pts3,pts3(2,:));
+% get anti-clockwise angles, in degrees, of vectors from (1,0)
+angs = atan2d(pts3([1,3],2),pts3([1,3],1));
+% return clockwise angle from first to second
+theta = angs(1) - angs(2);
+
